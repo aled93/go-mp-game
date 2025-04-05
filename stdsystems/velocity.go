33c80f31d0d84f0904120bfe81137c42961cfc28
milanjrodd <milanrodd@gmail.com>
2025-04-05 12:45:02 +0300

    feat velocity system asserts

--- stdsystems/velocity.go
+++ stdsystems/velocity.go
@@ -7,8 +7,10 @@ with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 package stdsystems
 
 import (
+	"github.com/negrel/assert"
 	"gomp/pkg/ecs"
 	"gomp/stdcomponents"
+	"math"
 	"time"
 )
 
@@ -29,7 +31,10 @@ func (s *VelocitySystem) Run(dt time.Duration) {
 
 	s.Velocities.EachEntity(func(e ecs.Entity) bool {
 		velocity := s.Velocities.Get(e)
+		assert.True(s.isVelocityValid(velocity))
+
 		position := s.Positions.Get(e)
+		assert.True(s.isPositionValid(position))
 
 		position.XY.X += velocity.X * dtSec
 		position.XY.Y += velocity.Y * dtSec
@@ -38,3 +43,51 @@ func (s *VelocitySystem) Run(dt time.Duration) {
 }
 
 func (s *VelocitySystem) Destroy() {}
+
+func (s *VelocitySystem) isVelocityValid(velocity *stdcomponents.Velocity) bool {
+	if velocity == nil {
+		return false
+	}
+
+	// Convert to float64
+	x := float64(velocity.X)
+	y := float64(velocity.Y)
+
+	if math.IsInf(x, 1) || math.IsInf(x, -1) {
+		return false
+	}
+
+	if math.IsInf(y, 1) || math.IsInf(y, -1) {
+		return false
+	}
+
+	if math.IsNaN(x) || math.IsNaN(y) {
+		return false
+	}
+
+	return true
+}
+
+func (s *VelocitySystem) isPositionValid(position *stdcomponents.Position) bool {
+	if position == nil {
+		return false
+	}
+
+	// Convert to float64
+	x := float64(position.XY.X)
+	y := float64(position.XY.Y)
+
+	if math.IsInf(x, 1) || math.IsInf(x, -1) {
+		return false
+	}
+
+	if math.IsInf(y, 1) || math.IsInf(y, -1) {
+		return false
+	}
+
+	if math.IsNaN(x) || math.IsNaN(y) {
+		return false
+	}
+
+	return true
+}
