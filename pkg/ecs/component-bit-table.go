17ae43ada746a892358ef9b8e298d372dbea23fb
bitver <bitver24@gmail.com>
2025-04-26 11:15:41 +0500

    feat: chunked bit table add: tests

--- pkg/ecs/component-bit-table.go
+++ pkg/ecs/component-bit-table.go
@@ -20,47 +20,66 @@ import (
 )
 
 const ComponentBitTablePreallocate = 1024
+const chunkBaseSize = ComponentBitTablePreallocate
+const chunkPreallocate = 2
 
 func NewComponentBitTable(maxComponentsLen int) ComponentBitTable {
-	bitsetSize := (maxComponentsLen / bits.UintSize) + 1
+	bitsetSize := ((maxComponentsLen - 1) / bits.UintSize) + 1
 	return ComponentBitTable{
-		bits:       make([]uint, bitsetSize, bitsetSize*ComponentBitTablePreallocate),
+		bits:       make([][]uint, 0, chunkPreallocate),
 		lookup:     make(map[Entity]int, ComponentBitTablePreallocate),
 		bitsetSize: bitsetSize,
 	}
 }
 
 type ComponentBitTable struct {
-	bits       []uint
+	bits       [][]uint
 	lookup     map[Entity]int
+	length     int
 	bitsetSize int
 }
 
 // Set sets the bit at the given index to 1.
 func (b *ComponentBitTable) Set(entity Entity, componentId ComponentId) {
 	bitsId, ok := b.lookup[entity]
-	if !ok { // Most likely the entity is new
+	if !ok {
 		b.extend()
-		bitsId = len(b.bits)
+		bitsId = b.length
 		b.lookup[entity] = bitsId
-		b.bits = append(b.bits, make([]uint, b.bitsetSize)...)
+		b.length += b.bitsetSize
 	}
 
+	chunkId := bitsId / chunkBaseSize
+	bitsetId := bitsId % chunkBaseSize
 	offset := int(componentId / bits.UintSize)
-	b.bits[bitsId+offset] |= 1 << (componentId % bits.UintSize)
+	b.bits[chunkId][bitsetId+offset] |= 1 << (componentId % bits.UintSize)
 }
 
 // Unset clears the bit at the given index (sets it to 0).
 func (b *ComponentBitTable) Unset(entity Entity, componentId ComponentId) {
 	bitsId, ok := b.lookup[entity]
 	assert.True(ok, "entity not found")
+	chunkId := bitsId / chunkBaseSize
+	bitsetId := bitsId % chunkBaseSize
 	offset := int(componentId / bits.UintSize)
-	b.bits[bitsId+offset] &= ^(1 << (componentId % bits.UintSize))
+	b.bits[chunkId][bitsetId+offset] &= ^(1 << (componentId % bits.UintSize))
+}
+
+func (b *ComponentBitTable) Test(entity Entity, componentId ComponentId) bool {
+	bitsId, ok := b.lookup[entity]
+	if !ok {
+		return false
+	}
+	chunkId := bitsId / chunkBaseSize
+	bitsetId := bitsId % chunkBaseSize
+	offset := int(componentId / bits.UintSize)
+	return (b.bits[chunkId][bitsetId+offset] & (1 << (componentId % bits.UintSize))) != 0
 }
 
 func (b *ComponentBitTable) extend() {
-	if len(b.bits) == cap(b.bits) {
-		b.bits = append(b.bits, make([]uint, cap(b.bits)*2)...)
+	lastChunkId := b.length / chunkBaseSize
+	if lastChunkId == len(b.bits) && b.length%chunkBaseSize == 0 {
+		b.bits = append(b.bits, make([]uint, b.bitsetSize*chunkBaseSize))
 	}
 }
 
@@ -69,9 +88,11 @@ func (b *ComponentBitTable) AllSet(entity Entity, yield func(ComponentId) bool) 
 	if !ok {
 		return
 	}
+	chunkId := bitsId / chunkBaseSize
+	bitsetId := bitsId % chunkBaseSize
 	for i := 0; i < b.bitsetSize; i++ {
 		for j := 0; j < bits.UintSize; j++ {
-			if (b.bits[bitsId+i]>>j)&1 == 1 {
+			if (b.bits[chunkId][bitsetId+i]>>j)&1 == 1 {
 				if !yield(ComponentId(i*bits.UintSize + j)) {
 					return
 				}
